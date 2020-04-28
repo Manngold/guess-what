044cb6053001e30fda98d25945954cd4e8056c04
Manngold <jsw4820@gmail.com>
2020-04-28 21:01:40 +0900

    implementation classifier

--- src/components/Classifier.js
+++ src/components/Classifier.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ml5 from "ml5";
 
@@ -11,10 +11,45 @@ const Image = styled.img`
     width: 30rem;
 `;
 
+const List = styled.ul``;
+
+const Item = styled.li``;
 function Classifier({ imageSrc }) {
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const classifyImg = () => {
+        const modelLoaded = () => setLoading(true);
+
+        const classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+        const image = document.querySelector("#image");
+
+        classifier
+            .predict(image, 5, (err, classifiedResults) => classifiedResults)
+            .then((classifiedResults) => setResults(classifiedResults))
+            .then(setLoading(false));
+    };
     return (
         <Container>
-            <Image src={imageSrc} />
+            <Image
+                src={imageSrc}
+                id="image"
+                onLoad={classifyImg}
+                crossOrigin="anonymous"
+            />
+            <List>
+                {results.map((result, index) => {
+                    const { label, confidence } = result;
+                    return (
+                        <Item key={index}>{`${
+                            index + 1
+                        }. Predictation : ${label} , ${Math.floor(
+                            confidence * 100
+                        )}%`}</Item>
+                    );
+                })}
+            </List>
         </Container>
     );
 }
