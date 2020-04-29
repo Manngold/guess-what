4ae5b2aad28eb16c69076a9d9d343206fc116417
Manngold <jsw4820@gmail.com>
2020-04-29 17:57:23 +0900

    Updates

---
+++ precache-manifest.b7fedcb62d5259b0e0e9ce1440e9fe75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a65af433b2b3245d561eb790f5c8374",
+    "url": "/guess-what/index.html"
+  },
+  {
+    "revision": "8ac4c5a1be37e047dd0e",
+    "url": "/guess-what/static/js/2.ab4a5181.chunk.js"
+  },
+  {
+    "revision": "26624b8331c21760682aea3e80184a25",
+    "url": "/guess-what/static/js/2.ab4a5181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e296ed642886df3eca",
+    "url": "/guess-what/static/js/main.53f85c34.chunk.js"
+  },
+  {
+    "revision": "217d547d8fecf39885f2",
+    "url": "/guess-what/static/js/runtime-main.6e0bf26a.js"
+  }
+]);
